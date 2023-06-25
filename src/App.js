5aa734ef8d1b5d41dba50dfb306fa81d5ece64e4
info <imranshaik2193@gmail.com>
2023-06-25 19:17:11 +0530

    add more

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import react from 'react';
+
+import ReactDom from 'react-dom';
+import './App.css';
+
+git 
+const App=()=>{
+
+  
+return( 
+  
+  <section>
+    <h3>Ishara Infotech</h3>
+    <h5><button className="container-button" >More...</button></h5>
+  </section>
+);
+};
+
+export default App;
---
+++ src/New.js
@@ -0,0 +1,10 @@
+
+
+const New =()=>{
+
+    return (
+        
+        <section> <h4> Excuse Me..!</h4></section>)
+}
+
+export default  New.js ;
